c4aeb5487d5aa5038a64c34340c8eb6c2d0b9b5c
Andrea <andreacabrera37@gmail.com>
2023-10-13 16:57:38 -0600

    agregando productos

--- src/components/product-list/product-list.tsx
+++ src/components/product-list/product-list.tsx
@@ -1,16 +1,19 @@
 import { Products } from "../../models/products-model.d";
 
 
-const  ProductList = ({products}: { products: Products[]}) => {
-    return (<>
-    <ul>
+const ProductList = ({ products }: { products: Products[] }) => {
+    if (!products || products.length === 0) {
+      return <p>No products available.</p>;
+    }
+  
+    return (
+      <ul>
         Choose one:
-        {
-            products.map((p) => (<li>{p.name}</li>))
-        }
-    </ul>
-    <div></div>
-    </>);
-};
-
-export default ProductList;
+        {products.map((product, index) => (
+          <li key={index}>{product.name}</li>
+        ))}
+      </ul>
+    );
+  };
+  
+  export default ProductList;
